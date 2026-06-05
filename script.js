9431572dfa6bd1e098a5969caa3c5dacb5f4c0bc
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-04 20:16:50 -0500

    added the loops and function to script

--- script.js
+++ script.js
@@ -1,96 +1,63 @@
-const obstaclesArray = ["Time: There seems to be never enough time to teach, grade, and just be a family, you have to find the right schedule.",
+const obstaclesArray = ["OBSTACLES", "Time: There seems to be never enough time to teach, grade, and just be a family, you have to find the right schedule.",
        "Financial and public resource loss: You are at risk of loosing after-school programs, special education resources, and even money when you decide to homeschool, so look for free and discounted resources, and which school teams allow homeschool children to play.",
       "Burnout: Yes even you as a homeschool parent will experience burnout from the stress an emotional fatigue like in any other profession, take breaks, learn to breathe through it, plan to have unplanned outings.",
       "Socialization: Yes while it can be great to target who you deem is right for you child(ren) it can rob them of important interactions, they can miss out on learning true problem solving skills from those that will push thier values and ways of."];
-const benefitsArray = ["Flexibale learning envrionment: The freedom to learn how you please from hours, days, or evenn the stucture.",
+const benefitsArray = ["BENEFITS","Flexibale learning envrionment: The freedom to learn how you please from hours, days, or evenn the stucture.",
       "The change of the pace: It is easier to pace your child(ren) acadaimic growth, you dicide when it is best to stay on a subject longer or move to the next.", 
         "Envrionment: This could free the stigma for a bully, distraction, or peer pressure free safe place.",
        "Targeted Social groups: you can set up inter-actions with like minded community, you become incontrol of peer groups and outings."];
 
     //    I want the loop to flip over like a flashcard
 
-    //    function flipContentLists(){
-    //     const obstacles = document.getElementById(obstacles-container);
-    //     const benefits = document.getElementById(benefits-container);
-    //     console.log(hi);
-
-    //     for (let i = 0; i < obstaclesArray.length; i++){
-    //     const card = document.createCard("obstacles")
-    //     card.className ="info-card obstacles-card";
-    //     card.textContent = obstaclesArray[i];
-    //     obstacles-Container.appendChild(card);
-    //    }
-
-    //    for (let j = 0; j < benefitsArray.length; j++){
-    //     const card = document.createCard("benefits")
-    //     card.className ="info-card benefits-card";
-    //     card.textContent = benefitsArray[i];
-    //     benefits-Container.appendChild(card);
-    //    }
-
-    //    renderContentLists();
-
-    //    };
-       
-    //    if they correctly enter a email they can unlock resource page
 
 const form = document.querySelector("#newsletter");
+
 form.onsubmit = handleSubmit;
+const output = document.getElementById("output");
 
-// const savedInfoString = localStorage.getItem("#userInfoString");
+function handleSubmit(event){
+    event.preventDefault();
+    const form = event.target;
 
-// const savedInfo = JSON.parse(savedInfoString);
-// const output = document.querySelector("output");
-// console.log("hi")
+    const userData = {
+        firstname: form.elements.firstname.value,
+        email: form.elements.email.value,
+    };
+    console.log(userData);
+    // add user info to local storage
+//   localStorage.setItem("userName","userData.username");
+//   localStorage.setItem("userEmail","userData.email");
+localStorage.setItem("userData", JSON.stringify(userData));
+const storedUserData = localStorage.getItem("userData");
+// console.log(storedUserData);
+
+if(storedUserData){
+    const userData = JSON.parse(storedUserData);
+     output.innerText = "You have successfully joined, " + userData.firstname + "!";  
+}else {
+     output.innerText = "Enter a valid email address."
+}
+
+};
+    
+const formTag = document.getElementById("affrimForm");
+formTag.onsubmit = handleSubmit;
 
 async function handleSubmit(event){
     event.preventDefault();
     const form = event.target;
 
-    const userData = {
-        username: form.elements.username.value,
-        email: form.elements.email.value,
-    };
-    console.log(userData);
-    // add user info to local storage
-  localStorage.setItem("userName","userData.username");
-  localStorage.setItem("userEmail","userData.email");
-    // const dataString = JSON.stringify(data);
-}
-    // const response = await fetch("https://dummyjson.com/auth/login",{
-//    method: "POST",
-
-//    headers: {"Content-Type": "application/json" },
-//    body: dataString,
-
-    // const result = await response.json();
-    // const message = result.message;
-    // const userInfo = {
-    //     firstName: result.firstName,
-    //     email: result.email,
-    // };
-
-    // output.innerText = "You have Successfully joined, " + savedInfo.firstName + "!";
-
-    // const userInfoString = JSON.stringify(userInfo);
-    // localStorage.setItem("userInfoString", userInfoString);
-     
-// }
-
-// const isChecked = form.elements.ischecked.checked
-
-// if(isChecked){
-//     checkbox.innerHtml = <a href="Resources.html">Resources</a>
+    const data = {"api-key": cb612559bbf74a23a8917dfff4cdbb96};
     
-// } else{
-//     innerText = "Enter valid email"
-// }
-// const form = document.querySelector(#form)
+    const queryString = new URLSearchParams(data);
+    const response = await fetch("https://www.affirmations.dev/" + queryString);
+    const result = await response.json();
+    const affirmation = result.affirmation;
+    const src = result.url;
+    console.log(affirmation);
 
-// const dataString = new URLSearchParams(data);
-// const response = await fetch("https://fastudy.app/learning-style-test");
-// const result = await response.json();
-// console.log(result);
 
-// const output = document.getElementById("output");
-// outut.innerText = "" + "is how I l";p
+    const outputTag = document.getElementById("affrimation");
+    outputTag.innerText = affirmation;
+}
+console.log("affrim");
